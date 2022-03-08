987f0ca074ffe5e2aef55b47cdcbfd89e973e67f
andreiBe <andrei.be05@gmail.com>
2022-03-08 23:26:32 +0200

    korjasin koko apin

--- src/Functions.js
+++ src/Functions.js
@@ -1,168 +1,149 @@
-import fetch from 'isomorphic-fetch'
-
+import fetch from "isomorphic-fetch";
 // Function to get menu. Async needed for await and ...other for extra args in future
-async function getMenu(UrlJSON, day, channel, ...other) {
-	// Fetch data and save it asynchronously to data. Await needed in order for software to write
-	let data = await fetch(UrlJSON)
-		.then((response) => {
-		    if (response.status >= 400) {
-		        throw new Error("Bad response from server");
-		    }
-		    return response.json();
-		})
-		.catch((e) => console.log(e))
+const days = ["Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai"];
 
-	// Find object for right day and store it to variable cut.
-	try {
-		var cut = data.Days.find((obj) => (obj.Date === day)).Meals;
-	}
-	catch (e) {
-		console.log(e);
-	}
+function field(name, value) {
+  return {
+    name: name,
+    value: value,
+    inline: true,
+  };
+}
 
-	//Main meal
-	let MainMeal = cut[0].Name.replace('Uunimakkara', 'UUUNIMAKKAARAAAAOUUUUYHJEAAAAHBOYYYYYYY') + "\n";
-
-	//Vegetarian
-	let SecondMeal = cut[1].Name + "\n";
-
-	//Send embbed message
-	channel.send({
-		embed: {
-			"color": 2134768,
-			"timestamp": new Date(),
-			"footer": {
-				"icon_url": "https://pbs.twimg.com/profile_images/441542471760097280/9sDmsLIm_400x400.jpeg",
-				"text": "© N Production. Hosted by Gaz, " + toHoliday()()
-			},
-			"fields": [
-				{
-					"name": "Lounas:",
-					"value": MainMeal,
-					"inline": true
-				},
-				{
-					"name": "Kasvislounas:",
-					"value": SecondMeal,
-					"inline": true
-				}
-			]
-		}
-	});
-};
-
-// This function returns correct JSON file for corresponding week. Requires valid RestaurantId. Should be used only with scheduling to avoid unnecessary resource usage.
-// Async needed for await and ...other for extra args in future
-async function SetCWeekMenuURL(RestaurantID, ...other) {
-	return new Promise(async (resolve, reject) => {
-  	// URL should be moved to config file.
-    let data = await fetch('https://ruokalistatkoulutjapaivakodit.arkea.fi/AromiStorage/blob/main/AromiMenusJsonData')
-      .then((response) => {
-          if (response.status >= 400) {
-              throw new Error("Bad response from server");
-          }
-          return response.json();
-      })
-      .catch((e) => console.log(e))
-
-    // Find correct restaurant with given RestaurantID.
-    let restaurant = data.Restaurants.find((obj) => (obj.RestaurantId === RestaurantID));
-		let today;
-		let date = other.find((obj) => (typeof(Date)));
-		if(date !== undefined) {
-		    today = new Date(date);
-		} else {
-		    today = new Date();
-		}
-
-    if(restaurant !== undefined) {
-        restaurant.JMenus.map((obj) => {
-          let start = new Date(obj.Start);
-          let end = new Date(obj.End);
-          if(today >= start && today <= end) {
-	          let LinkJSON = obj.LinkUrl;
-	          resolve(LinkJSON);
-          }
-      })
-    } else {
-      reject("Invalid restaurant ID");
-    }
-
+async function getMenu(data, channel) {
+  //list that contains fields for discord api
+  const list = [];
+  days.forEach((day) => {
+    list.push(field("Viikonpäivä", day));
+    list.push(field("Lounas: ", data[day].liha));
+    list.push(field("Kasvislounas: ", data[day].kasvis));
+  });
+  //Send embbed message
+  channel.send({
+    embed: {
+      color: 0x3fc77,
+      timestamp: new Date(),
+      footer: {
+        icon_url:
+          "https://pbs.twimg.com/profile_images/441542471760097280/9sDmsLIm_400x400.jpeg",
+        text: "Ruokalista",
+      },
+      fields: list,
+    },
   });
 }
 
-const days = ["Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai"]
+// This function returns correct JSON file for corresponding week. Requires valid RestaurantId. Should be used only with scheduling to avoid unnecessary resource usage.
+// Async needed for await and ...other for extra args in future
+import { parse } from "node-html-parser";
+async function SetCWeekMenuURL(RestaurantID, ...other) {
+  return new Promise(async (resolve, reject) => {
+    let data = null;
+    try {
+      data = await fetch(
+        "https://masu.arkea.fi/fi/lounaslistat?restaurant=711af04e-e0d0-4e28-9a32-cacbe8504150"
+      );
+    } catch (error) {
+      reject("Can't open arkea page");
+      return;
+    }
 
-const getEatingTime = ( atmClass, data ) => {
-	if(atmClass === undefined) {
-		return "Class needed"
-	}
-	let n = new Date().getDay() - 1
-	if (n < 0 || n >= 5) {
-		return "Invalid day"
-	}
-	const day = days[n]
-	let times = data[day]
-	for (let time in times) {
-		if(times[time].toLowerCase().includes(atmClass.toLowerCase())) {
-			return time
-		}
-	}
-	return "Can't find that class for that day"
+    const htmlString = await data.text();
+
+    const html = parse(htmlString);
+    const mydata = {};
+    let days = html.querySelectorAll(".day");
+    days.forEach((day) => {
+      let ruoatDivs = day.querySelectorAll(".menu-item");
+      let weekdayname = day.querySelector(".weekday").text;
+
+      let foodsLiha = ruoatDivs[0].querySelectorAll(".name");
+      let foodsKasvis = ruoatDivs[1].querySelectorAll(".name");
+
+      const liharuoka = foodsLiha[0].text.replace("\n", " ").trim();
+      const kasvisruoka = foodsKasvis[0].text.replace("\n", " ").trim();
+
+      console.log(liharuoka + " " + kasvisruoka + " " + weekdayname);
+
+      mydata[weekdayname] = {
+        liha: liharuoka,
+        kasvis: kasvisruoka,
+      };
+    });
+    resolve(mydata);
+  });
 }
+
+const getEatingTime = (atmClass, data) => {
+  if (atmClass === undefined) {
+    return "Class needed";
+  }
+  let n = new Date().getDay() - 1;
+  if (n < 0 || n >= 5) {
+    return "Invalid day";
+  }
+  const day = days[n];
+  let times = data[day];
+  for (let time in times) {
+    if (times[time].toLowerCase().includes(atmClass.toLowerCase())) {
+      return time;
+    }
+  }
+  return "Can't find that class for that day";
+};
 
 const toHoliday = () => {
-	let date1 = new Date();
-	let syysloma = new Date("2018-10-15T12:00:00+02:00");
-	let joululoma = new Date("2018-12-23T12:00:00+02:00");
-	let talviloma = new Date("2019-02-18T12:00:00+02:00");
-	let kesaloma = new Date("2019-06-02T12:00:00+02:00");
-	let timeDiff = 0;
-	let diffDays = 0;
-	return (loma) => {
-		if(loma) {
-			switch (loma) {
-				case 'joululoma':
-					timeDiff = Math.abs(joululoma.getTime() - date1.getTime());
-					diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-					return (diffDays-1) + " päivää joululomaan"
-					break;
-				case 'talviloma':
-					timeDiff = Math.abs(talviloma.getTime() - date1.getTime());
-					diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-					return (diffDays-1) + " päivää talvilomaan"
-					break;
-				case 'kesäloma':
-					timeDiff = Math.abs(kesaloma.getTime() - date1.getTime());
-					diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-					return (diffDays-1) + " päivää kesälomaan"
-					break;
-				default:
-					if(syysloma.getTime() - date1.getTime() > 0) {
-						timeDiff = Math.abs(syysloma.getTime() - date1.getTime());
-						diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-						return (diffDays-1) + "päivää syyslomaan"
-					} else if(joululoma.getTime() - date1.getTime() > 0) {
-						timeDiff = Math.abs(joululoma.getTime() - date1.getTime());
-						diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-						return (diffDays-1) + " päivää joululomaan"
-					} else if(talviloma.getTime() - date1.getTime() > 0) {
-						timeDiff = Math.abs(talviloma.getTime() - date1.getTime());
-						diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-						return (diffDays-1) + " päivää talvilomaan"
-					} else {
-						timeDiff = Math.abs(kesaloma.getTime() - date1.getTime());
-						diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-						return (diffDays-1) + " päivää kesälomaan"
-					}
-			}
-		} else {
-			timeDiff = Math.abs(kesa.getTime() - date1.getTime());
-			diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-			return (diffDays-1) + " kesälomaan"
-		}
-	}
-}
+  let date1 = new Date();
+  let syysloma = new Date("2018-10-15T12:00:00+02:00");
+  let joululoma = new Date("2018-12-23T12:00:00+02:00");
+  let talviloma = new Date("2019-02-18T12:00:00+02:00");
+  let kesaloma = new Date("2019-06-02T12:00:00+02:00");
+  let timeDiff = 0;
+  let diffDays = 0;
+  return (loma) => {
+    if (loma) {
+      switch (loma) {
+        case "joululoma":
+          timeDiff = Math.abs(joululoma.getTime() - date1.getTime());
+          diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+          return diffDays - 1 + " päivää joululomaan";
+          break;
+        case "talviloma":
+          timeDiff = Math.abs(talviloma.getTime() - date1.getTime());
+          diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+          return diffDays - 1 + " päivää talvilomaan";
+          break;
+        case "kesäloma":
+          timeDiff = Math.abs(kesaloma.getTime() - date1.getTime());
+          diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+          return diffDays - 1 + " päivää kesälomaan";
+          break;
+        default:
+          if (syysloma.getTime() - date1.getTime() > 0) {
+            timeDiff = Math.abs(syysloma.getTime() - date1.getTime());
+            diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+            return diffDays - 1 + "päivää syyslomaan";
+          } else if (joululoma.getTime() - date1.getTime() > 0) {
+            timeDiff = Math.abs(joululoma.getTime() - date1.getTime());
+            diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+            return diffDays - 1 + " päivää joululomaan";
+          } else if (talviloma.getTime() - date1.getTime() > 0) {
+            timeDiff = Math.abs(talviloma.getTime() - date1.getTime());
+            diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+            return diffDays - 1 + " päivää talvilomaan";
+          } else {
+            timeDiff = Math.abs(kesaloma.getTime() - date1.getTime());
+            diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+            return diffDays - 1 + " päivää kesälomaan";
+          }
+      }
+    } else {
+      timeDiff = Math.abs(kesa.getTime() - date1.getTime());
+      diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+      return diffDays - 1 + " kesälomaan";
+    }
+  };
+};
 
 // Export all functions
-export {getMenu, SetCWeekMenuURL, getEatingTime, toHoliday}
+export { getMenu, SetCWeekMenuURL, getEatingTime, toHoliday };
--- src/index.js
+++ src/index.js
@@ -1,91 +1,112 @@
 //Requirements
-import Discord from 'discord.js';
-import config from './config.json';
-import {getMenu, SetCWeekMenuURL, getEatingTime, toHoliday} from './Functions.js';
-import {returnThisDay, ConvertToISO, saveMessage} from './Utility.js';
-import responses from './responses.json';
-import schedule from 'node-schedule';
-import data from '../json/timetable.json';
-import specialData from '../json/poikkeusTimetable.json';
-import mongoose from 'mongoose';
+import { Client } from "discord.js";
+import config from "./config.json";
+import schedule from "node-schedule";
+import {
+  getMenu,
+  SetCWeekMenuURL,
+  getEatingTime,
+  toHoliday,
+} from "./Functions.js";
+/*
+import { returnThisDay, ConvertToISO, saveMessage } from "./Utility.js";
+import responses from "./responses.json";
 
+import data from "../json/timetable.json";
+import specialData from "../json/poikkeusTimetable.json";
+import mongoose from "mongoose";
+*/
 // Needed for async to work
 require("babel-core/register");
 require("babel-polyfill");
 
-const bot = new Discord.Client();
+const bot = new Client();
 
-bot.login(config.token)
-.catch((e) => {
-	console.log(e);
-})
+bot.login(config.token).catch((e) => {
+  console.log(e);
+});
 
 //Informs successful login to the console
-bot.on('ready', () => {
-	console.log('Connected');
-	console.log("Bot has started. Logged in as " + bot.user.username + ". Connected to " + bot.guilds.size + " servers");
-	bot.user.setGame(config.currentGame);
+bot.on("ready", () => {
+  console.log("Connected");
+  console.log(
+    "Bot has started. Logged in as " +
+      bot.user.username +
+      ". Connected to " +
+      bot.guilds.size +
+      " servers"
+  );
+  bot.user.setGame("Eating uunimakkara");
 });
 
 //Schedules, updates every day at 7:00AM. Fetches JSON file from Arkea website and extracts the information. Prints corresponding information for each day.
-let j = schedule.scheduleJob('0 7 * * *', async () => {
-	// Empty config.json to gitignore plsss
- 	let channel = bot.guilds.get(config.guildID).channels.get(config.menuChannelID);
-	let result = await SetCWeekMenuURL(config.restaurantID);
-	let day = returnThisDay();
-	getMenu(result, day, channel);
+let j = schedule.scheduleJob("0 7 * * MON", async () => {
+  config.guildIDs.forEach(async (val, i) => {
+    let channel = bot.guilds.get(val).channels.get(config.menuChannelIDs[i]);
+    let error = false;
+    let result = await SetCWeekMenuURL(config.restaurantID).catch((e) => {
+      error = true;
+    });
+    if (error || !channel) {
+      if (channel) channel.send("Error opening arkeas page");
+    }
+    getMenu(result, channel);
+  });
 });
 
-bot.on('message', async (message) => {
-	let content = message.content.toLowerCase();
-	if (content.substring(0, 1) == config.prefix) {
-		let args = content.substring(1).split(' ');
-		let cmd = args[0];
+bot.on("message", async (message) => {
+  let content = message.content.toLowerCase();
+  if (content.length > 0 && content.substring(0, 1) == config.prefix) {
+    let args = content.substring(1).split(" ");
+    let cmd = args[0];
 
-		//List of awailable commands
-		switch(cmd) {
-			//Random command, mainly for test purposes
-			case 'test':
-				message.channel.send("Hello!");
-				break;
+    //List of awailable commands
+    switch (cmd) {
+      //Random command, mainly for test purposes
+      case "test":
+        message.channel.send("Hello!");
+        break;
+      case "help":
+        message.channel.send("Commands:");
+        message.channel.send(config.prefix + "ruokalista");
+        break;
+      case "ruokalista":
+        const index = config.guildIDs.findIndex(
+          (gu) => gu === message.guild.id
+        );
 
-			//WIP
-			case 'help':
-				message.channel.send("Commands:");
-				//message.channel.send(message.channel);
-				break;
-
-			//WIP
-			case 'menu':
-				let day = ConvertToISO(args[1]);
-				let result = await SetCWeekMenuURL(config.restaurantID, day);
-				await getMenu(result, day, message.channel);
-				break;
-
-			case 'till':
-				if(args[1])
-					message.channel.send(toHoliday()(args[1]))
-				else
-					message.channel.send(toHoliday()())
-				break;
-
-			case 'time':
-				let atmClass = args[1]
-				let special = false
-				if(args[2] !== undefined && args[2] === "poikkeus")
-					special = true
-				let time = ""
-				if(special)
-					time = getEatingTime(atmClass, specialData)
-				else
-					time = getEatingTime(atmClass, data)
-				message.channel.send(time)
-		}
-	}
+        let channel = message.guild.channels.get(config.menuChannelIDs[index]);
+        let error = false;
+        let result = await SetCWeekMenuURL(config.restaurantID).catch((e) => {
+          error = true;
+        });
+        if (!channel || error) {
+          if (channel) channel.send("Error with opening arkeas page");
+          break;
+        }
+        getMenu(result, channel);
+        break;
+      /*
+      case "till":
+        if (args[1]) message.channel.send(toHoliday()(args[1]));
+        else message.channel.send(toHoliday()());
+        break;
+      case "time":
+        let atmClass = args[1];
+        let special = false;
+        if (args[2] !== undefined && args[2] === "poikkeus") special = true;
+        let time = "";
+        if (special) time = getEatingTime(atmClass, specialData);
+        else time = getEatingTime(atmClass, data);
+        message.channel.send(time);
+      */
+    }
+  }
 });
-
+/*
 //To "star" message when star reaction is added
-bot.on('messageReactionAdd', (reaction, user) => {
-	if (reaction.emoji == "⭐" && reaction.message.guild)
-		saveMessage(user, reaction)
+bot.on("messageReactionAdd", (reaction, user) => {
+  if (reaction.emoji == "⭐" && reaction.message.guild)
+    saveMessage(user, reaction);
 });
+*/
