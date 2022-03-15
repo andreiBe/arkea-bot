21bbcf21831dcf052f15441290581ab543955fe9
EinoFin <101316567+EinoFin@users.noreply.github.com>
2022-03-15 10:49:23 +0200

    Update Functions.js

--- src/Functions.js
+++ src/Functions.js
@@ -22,7 +22,7 @@ async function getMenu(data, channel) {
   //Send embbed message
   channel.send({
     embed: {
-      color: 0x3fc77, //green
+      color: 0x3fc77, //vihreä
       timestamp: new Date(),
       footer: {
         //arkea icon
@@ -34,13 +34,14 @@ async function getMenu(data, channel) {
     },
   });
 }
+//This function returns an object for current week. Requires valid RestaurantId.
 import { parse } from "node-html-parser";
-//asettaa data muuttujaan yhden päivän tiedot. Parametrina annetaan sivuston päivää kuvaava html elementti.
 function parseDayElement(day, data) {
   //Eri ruoka aikoja varten eri laatikko. Esim: lounas, aamupala, välipala, kasvislounas
   let ruoatDivs = day.querySelectorAll(".menu-item");
   let weekdayname = day.querySelector(".weekday").text;
 
+  //tallentaa
   let foodsLiha = undefined;
   let foodsKasvis = undefined;
 
@@ -74,30 +75,21 @@ let cached = {};
 function clearCache() {
   cached = {};
 }
-// Returns the ISO week of the date. (kopioitu netistä en ole näin viisas)
-Date.prototype.getWeek = function () {
+// Returns the ISO week of the date.
+Date.prototype.getWeek = function() {
   var date = new Date(this.getTime());
   date.setHours(0, 0, 0, 0);
   // Thursday in current week decides the year.
-  date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
   // January 4 is always in week 1.
   var week1 = new Date(date.getFullYear(), 0, 4);
   // Adjust to Thursday in week 1 and count number of weeks from date to week1.
-  return (
-    1 +
-    Math.round(
-      ((date.getTime() - week1.getTime()) / 86400000 -
-        3 +
-        ((week1.getDay() + 6) % 7)) /
-        7
-    )
-  );
-};
-//lukee arkean sivulta tiedot ja palauttaa tiedot
+  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+                        - 3 + (week1.getDay() + 6) % 7) / 7);
+}
 async function SetCWeekMenuURL(restaurantID) {
   return new Promise(async (resolve, reject) => {
     try {
-      //on jo haettu, niin ei ole mitään syytä hakea uudestaan
       if (restaurantID in cached) {
         resolve(cached[restaurantID]);
         return;
@@ -120,7 +112,7 @@ async function SetCWeekMenuURL(restaurantID) {
       } else {
         days = nextweek.querySelectorAll(".day");
       }
-
+      
       days.forEach((day) => {
         parseDayElement(day, mydata);
       });
